336168e436a231a6a0c0eda494aec1f50a2119e1
Stephanie Zou <stephanie.zou@stern.nyu.edu>
2020-02-24 16:13:25 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -60,7 +60,6 @@ def dennis_ritchies_language
 end
 
 def adding_matz
-end
 # add the following information to the top level of programmer_hash
 # :yukihiro_matsumoto => {
 #   :known_for => "Ruby",
